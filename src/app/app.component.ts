d47300d1be5fde9eff58daffd6491cbaf4d10b1c
umberto46x <daniele.u.nicosia.dev@gmail.com>
2025-05-08 10:16:50 +0200

    added ChangeDetectionRef + managing changeDetectionStrategy on objects (array in this example)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,11 +12,13 @@ import { CommonModule } from '@angular/common';
     <button (click)="visible = !visible">Show / Hide </button>
     <button (click)="changeId()">Change id {{id}}</button>
     <button (click)="doNothing()">do nothing </button>
+    <!-- gestione della changeDetectionStrategy su un oggetto --> 
+    <button (click)="addValue()">add value to array </button>
     <input type="text" (keydown)="doNothing()" placeholder="doNothingByTyping" />
 
 
    
-    <app-user-profile [id]="id" [visible]="visible"  />
+    <app-user-profile [id]="id" [visible]="visible" [inputArray] = "inputArray"  />
  
     
     <router-outlet />
@@ -29,6 +31,9 @@ export class AppComponent {
   title = 'angular-lifecycle-change-detection-strategy';
   id: number= 1;
   visible: boolean = true;
+
+    // gestione della changeDetectionStrategy su un oggetto 
+  inputArray: number[] = [1,2,3,4] 
 
   constructor(){
     console.log("COnstructor app root");
@@ -45,5 +50,13 @@ export class AppComponent {
     }else{
       this.id++;
     }
+  }
+
+    // gestione della changeDetectionStrategy su un oggetto 
+  addValue(){
+    // this.inputArray.push(this.inputArray[this.inputArray.length -1] +1)
+    this.inputArray = [...this.inputArray,this.inputArray[this.inputArray.length -1] +1]
+    console.log("value added to array:",this.inputArray);
+
   }
 }
--- src/app/shared/user-profile.component.ts
+++ src/app/shared/user-profile.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { ChangeDetectionStrategy, Component, inject, Input, input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, Input, input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { User } from '../../models/User';
 
@@ -11,6 +11,8 @@ import { User } from '../../models/User';
     <p>
       user-profile works!
     </p>
+<!--     gestione della changeDetectionStrategy su un oggetto  -->
+    <pre>{{inputArray | json}}</pre>
     <pre>{{user | json}}</pre>
     <pre>{{visible}}</pre>
 
@@ -20,15 +22,21 @@ import { User } from '../../models/User';
 })
 export class UserProfileComponent implements OnInit,OnChanges,OnDestroy {
 
-  @Input() id: number | undefined
-  @Input() visible: boolean | undefined
+  @Input() id: number | undefined;
+  @Input() visible: boolean | undefined;
+
+  // gestione della changeDetectionStrategy su un oggetto 
+  @Input() inputArray: number[] | undefined;
 
 //  timer : ReturnType<typeof setTimeout> | undefined;
   http = inject(HttpClient);
   user: User | undefined
 
-  constructor(){
+
+  constructor(public cdr: ChangeDetectorRef){
     console.log("constructor id = ",this.id);
+        
+
 
     /* this.timer = setInterval(() => {
       console.log("timer");
@@ -40,18 +48,25 @@ export class UserProfileComponent implements OnInit,OnChanges,OnDestroy {
     console.log("OnChanges visible = ",this.visible);
   
     console.log("OnChanges changes = ", changes);
-
+  
     if (changes["id"]){
       this.http
       .get<User>(`https://jsonplaceholder.typicode.com/users/${changes["id"].currentValue}`)
       .subscribe( res => {
         console.log("res: ",res);
         this.user = res
+                // ChangeDetectorRef è utilizzato per forzare l'aggiornamento dei componenti. senza che ci siano modifiche
+                this.cdr.markForCheck();
       })
     }
 
     if(changes["visible"]){
       console.log("OnChanges visible = ",this.visible);
+
+    }
+
+    if(changes["inputArray"]){
+      console.log("OnChanges inputArray = ",this.inputArray);
 
     }
 
